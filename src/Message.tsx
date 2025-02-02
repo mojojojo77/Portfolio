f141de081a984f4ee1698da9a7f8e8b65e7e3d71
Arnav S. <arnav.saraf777@gmail.com>
2025-02-01 18:39:05 -0800

    Initial commit of the react app

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+// Pascal or CamelStyel casing
+function Message(){
+    // JSX: Javascript XML 
+    const name = "Arnav";
+    if(name)
+        return <h1>Hello {name}</h1>;
+    else 
+        return <h1>Hello World</h1>
+}
+
+export default Message;
